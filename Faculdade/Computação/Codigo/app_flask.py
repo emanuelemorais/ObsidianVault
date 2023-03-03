61ae27efcdc312de81a0915e3246cbb61fe4be15
Emanuele Morais <emanuele.lmorais@gmail.com>
2023-03-02 21:25:38 -0300

    aprendendo flask

--- Faculdade/Computação/Codigo/app_flask.py
+++ Faculdade/Computação/Codigo/app_flask.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from evento import Evento
+
+eventos = [
+    Evento(nome="Almoço", local="Lancheco", data="hoje")
+]
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def hello():
-    return render_template("index.html")
+    return render_template("index.html", nome_desenvolvedor="Emanuele Morais")
+
+@app.route("/novo")
+def cadastrar_evento():
+    return render_template("cadastrar.html")
+
+@app.route("/salvar", methods=['POST'])
+def cria_evento():
+    nome = request.form['nome']
+    data = request.form['data']
+    local = request.form['local']
+    evento = Evento(nome=nome, data=data, local=local)
+    global eventos
+    eventos.append(evento)
+    print(eventos)
+    return redirect('/') #direciona para rota /
+    
 
 app.run(host="0.0.0.0", port=3000) #Indica para que o flask execute as conexões na rede local e na porta 3000.
 
--- Faculdade/Computação/Python - Flask.md
+++ Faculdade/Computação/Python - Flask.md
@@ -7,3 +7,7 @@ python -m venv .
 
 python -m pip install flask
 ```
+
+- Paginas html necessitam estar dentro de uma pasta chamada templates para usar o render_template
+
+- Para criar um botão que chama outra rota é possivel colocar uma action
